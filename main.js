75f83715d91c2d0776cf7126ef73d7d9d408f9e8
zynb <zeinabghodousizadeh@gmail.com>
2024-07-22 14:48:38 +0330

    Create project

---
+++ main.js
@@ -0,0 +1,79 @@
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+const btn6 = document.getElementById("btn6");
+const btn7 = document.getElementById("btn7");
+const btn8 = document.getElementById("btn8");
+const btn9 = document.getElementById("btn9");
+const btn10 = document.getElementById("btn10");
+const btn11 = document.getElementById("btn11");
+const btn12 = document.getElementById("btn12");
+
+const text = document.getElementById("txt");
+const hero = document.getElementById("hero");
+
+btn1.addEventListener("click",function(){
+    hero.style.backgroundColor = "wheat";
+    btn1.textContent= "wheat";
+    btn1.style.backgroundColor="wheat";
+})
+btn2.addEventListener("click",function(){
+    hero.style.backgroundColor = "turquoise";
+    btn2.textContent= "turquoise";
+    btn2.style.backgroundColor="turquoise";
+})
+btn3.addEventListener("click",function(){
+    hero.style.backgroundColor = "orchid";
+    btn3.textContent= "orchid";
+    btn3.style.backgroundColor="orchid";
+})
+btn4.addEventListener("click",function(){
+    hero.style.backgroundColor = "palegreen";
+    btn4.textContent= "palegreen";
+    btn4.style.backgroundColor="palegreen";
+})
+btn5.addEventListener("click",function(){
+    hero.style.backgroundColor = "orange";
+    btn5.textContent= "orange";
+    btn5.style.backgroundColor="orange";
+})
+btn6.addEventListener("click",function(){
+    hero.style.backgroundColor = "crimson";
+    btn6.textContent= "crimson";
+    btn6.style.backgroundColor="crimson";
+})
+btn7.addEventListener("click",function(){
+    hero.style.backgroundColor = "rebeccapurple";
+    btn7.textContent= "rebeccapurple";
+    btn7.style.backgroundColor="rebeccapurple";
+})
+btn8.addEventListener("click",function(){
+    hero.style.backgroundColor = "plum";
+    btn8.textContent= "plum";
+    btn8.style.backgroundColor="plum";
+})
+btn9.addEventListener("click",function(){
+    hero.style.backgroundColor = "grey";
+    btn9.textContent= "grey";
+    btn9.style.backgroundColor="grey";
+})
+btn10.addEventListener("click",function(){
+    hero.style.backgroundColor = "lightblue";
+    btn10.textContent= "lightblue";
+    btn10.style.backgroundColor="lightblue";
+})
+btn11.addEventListener("click",function(){
+    hero.style.backgroundColor = "mediumpurple";
+    btn11.textContent= "mediumpurple";
+    btn11.style.backgroundColor="mediumpurple";
+})
+btn12.addEventListener("click",function(){
+    hero.style.backgroundColor = "hotpink";
+    btn12.textContent= "hotpink";
+    btn12.style.backgroundColor="hotpink";
+})
+
+
+
